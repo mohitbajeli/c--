63c1d46f018587b15fd3066a22aab00a29e7825f
mohitbajeli <mohitbajeli7617@gmail.com>
2022-11-09 00:14:50 +0530

    commit

--- stl/cbnst/iteration.c
+++ stl/cbnst/iteration.c
@@ -1,58 +1,68 @@
-//C Program to Implement Newton Raphson Method
+//C Program to Implement Iteration Method
 #include<stdio.h>
-#include<math.h> 
-#define EPSILON 0.0001
+#include<math.h>
+#define EPSILON 0.001
 
 float f(float x)
 {
-    return (x*tan(x))+1;
+    return x*x*x + x*x -1;
 }
 
+float findValueAt(float x)
+{
+    return 1/sqrt(1+x);
+}
 float differentiate(float x)
 {
-    return tan(x)+(x*(1/(cos(x)*cos(x))));
+    return 1/2.0 * (pow(1,pow((1+x),3/2.0)));
 }
 
 int main()
 {
     int maxIteration,i;
-    float x1,x2,x,x0;
+    float a,b,x,x0;
     printf("Enter Maximum no of Iterations\n");
     scanf("%d",&maxIteration);
 
-//......Compute x1 and x2............. 
+//......Compute a and b............. 
    
    do
    {
-        printf("Enter the value of x1 and x2(starting boundary)");
-        scanf("%f%f",&x1,&x2);
-        if(f(x1)*f(x2)>0)
+        printf("Enter the value of a and b(starting boundary)");
+        scanf("%f%f",&a,&b);
+        if(f(a)*f(b)>0)
         {
            printf("Boundary Values are Invalid\n");
            continue;
         }
         else
         {
-            printf("Roots Lie between %f and %f\n",x1,x2);
+            printf("Roots Lie between %f and %f\n",a,b);
             break;
         }
     } while(1);
 
     //find x0
-    if(fabs(f(x1)) < fabs(f(x2)))
-        x0 = x1;
+    x0= (a+b)/2;
+    //check if the function form used is valid or invalid
+    if(fabs(differentiate(x0))<1)
+        printf("Function Form is Correct. Iteration Method can be applied\n");
     else
-        x1 = x2;    
+    {
+        printf("Function Form is NOT Correct. Iteration Method CAN'T be applied\n");
+        return 0;
+    }
 
-    //Apply Successive approximation to find the root b/w x1 and x2
+    //Apply Successive approximation to find the root b/w a and b
     //..........Find root............   
+    
     for(i=1;i<=maxIteration;i++)
     {
-      x = x0 - (f(x0)/differentiate(x0));  
+      x = findValueAt(x0);   //find the mid point
       
       if(fabs(x-x0)<EPSILON)
-      {       
-            printf("Iterations=%d  Final Root=%f\n",i,x);
+      {
+           printf("Iterations=%d  Final Root=%f\n",i,x);
            return 0;
       }      
       printf("Iterations=%d  Roots=%f\n",i,x); 
