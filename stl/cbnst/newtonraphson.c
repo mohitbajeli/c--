4b8b969d70e07bb762931853f5f3d9b4bd9f8211
mohitbajeli <mohitbajeli7617@gmail.com>
2022-11-08 18:11:34 +0530

    commit

--- stl/cbnst/newtonraphson.c
+++ stl/cbnst/newtonraphson.c
@@ -1,44 +1,53 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-double function(double x)
-{
+double function(double x){
     return x*tan(x)+1;
+    //return x*x*x - 3*x -5;
+    //return x*sin(x) + cos(x);
 }
 
-double derivative(double x)
-{
-    return tan(x)+x*(1/(cos(x)*cos(x)));;
+double derivative(double x){
+    return tan(x)+x*(1/(cos(x)*cos(x)));
+   // return 3*x*x - 3;
+    //return x*cos(x);
 }
 
-double ex(double x)
-{
-    return (x-(function(x)/derivative(x)));
+double ex(double x){
+    return (x - (function(x)/derivative(x)));
 }
 
-int main()
-{
-    double a,b,x,x1;
-    int steps = 0;
-    for(int i=-5;i<5;i++)
-    {
+int main(){
+    double a, b;
+
+    for (int i = 0; i <= 5; i++){
         a = i;
         b = i+1;
-        if(function(a)*function(b)<0)
-        {
+        if (function(a)*function(b) < 0){
             break;
         }
     }
-    printf("a = %lf b = %lf\n",a,b);
-    x = (a+b)/2;
-    do
-    {
-       x1 = x;
-       x = ex(x);
-       printf("iteration = %d x = %.4lf\n",++steps,x);
 
-    }while(fabs(x-x1)>=0.0001);
-    printf("root is %.4lf\n",x);
-    return 0;
-    
+/*
+    printf("Enter the value of a: ");
+    scanf("%lf",&a);
+    printf("Enter the value of b: ");
+    scanf("%lf",&b);
+*/
+
+    printf("a : %lf ,  b : %lf\n", a, b);
+
+    double x = (a+b)/2;
+    double x1;
+    int step = 0;
+
+    do{
+        x1 = x;
+        x = ex(x);
+        
+        printf("Interation %d, x : %lf\n", ++step, x);
+
+    }while(fabs(x-x1) >= 0.001);
+
+    printf("x : %lf\n", x);
 }
