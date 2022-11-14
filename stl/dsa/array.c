84d05bb4fdfa9574932d77c6adb821fa237552a7
mohitbajeli <mohitbajeli7617@gmail.com>
2022-11-14 22:35:25 +0530

    commit

--- stl/dsa/array.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int arr[34]={12,65,43,67,23};
-    int *q=arr;
-    printf("the element is %d\n",*(arr+2));
-    printf("the element is %d",*(q+2));
-    return 0;
-}
--- stl/dsa/destructor practide.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-  int a=90;
-  int *ptr;
-  ptr=&a;
-}
