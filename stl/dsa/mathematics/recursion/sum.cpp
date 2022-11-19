dfac6a1e544859c8d6b721569cfc553b3cdad296
mohitbajeli <mohitbajeli7617@gmail.com>
2022-11-19 18:01:26 +0530

    commit

---
+++ stl/dsa/mathematics/recursion/sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sum(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+
+    return n+sum(n-1);
+}
+
+int main()
+{
+    int n;
+    cout<<"enetr the no of sum"<<endl;
+    cin>>n;
+    cout<<"the sum of "<<n<<" no is "<<sum(n);
+    return 0;
+}
