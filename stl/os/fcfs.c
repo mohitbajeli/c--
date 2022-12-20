5ab64c747265d42b2a0b41c122448de40d9b19c2
mohitbajeli <mohitbajeli7617@gmail.com>
2022-12-20 17:52:59 +0530

    c

--- stl/os/fcfs.c
+++ stl/os/fcfs.c
@@ -14,85 +14,87 @@ int findmax(int a,int b)
   return a>b?a:b;
 }
 
-int comparatorAT(const void *a,const void *b)
+int comparatorAT(void const *a,void const *b)
 {
   int x = ((struct process_struct *)a)->at;
   int y = ((struct process_struct *)b)->at;
 
   if(x<y)
   return -1;
-  if(x>=y)
+  else if(x>=y)
   return 1;
 }
 
-int comparatorPID(const void *a,const void *b)
+int comparatorPID(void const *a,void const *b)
 {
   int x = ((struct process_struct *)a)->pid;
   int y = ((struct process_struct *)b)->pid;
 
   if(x<y)
   return -1;
-  if(x>=y)
+  else if(x>=y)
   return 1;
 }
 
 int main()
 {
   int n;
-  float sum_tat =0,sum_rt = 0,sum_wt=0;
-  float length_cycle,idle_time=0,cpu_utilisation;
-  float throughput;
-  printf("enter the no of processes\n");
+  float sum_tat=0,sum_wt=0,sum_rt=0;
+  float length_cycle,cpu_utilisation,throughput;
+  int idle_time=0;
+
+  printf("enter the no of process\n");
   scanf("%d",&n);
 
   for(int i=0;i<n;i++)
   {
-    printf("enter %d process arrival time : ",i);
+    printf("enter %d process arrival time\n",i);
     scanf("%d",&ps[i].at);
     ps[i].pid=i;
   }
 
   for(int i=0;i<n;i++)
   {
-    printf("enter %d process burst time : ",i);
+    printf("enter %d process burst time\n",i);
     scanf("%d",&ps[i].bt);
-    
   }
 
-  qsort((void *)ps,n, sizeof(struct process_struct),comparatorAT);
+  qsort((void *)ps,n,sizeof(struct process_struct),comparatorAT);
 
   for(int i=0;i<n;i++)
   {
     ps[i].start_time = (i==0)?ps[i].at:findmax(ps[i].at,ps[i-1].ct);
+    ps[i].ct = ps[i].start_time+ps[i].bt;
+    ps[i].tat = ps[i].ct-ps[i].at;
+    ps[i].wt = ps[i].tat-ps[i].bt;
+    ps[i].rt=ps[i].wt;
 
-    ps[i].ct = ps[i].start_time + ps[i].bt;
-    ps[i].tat = ps[i].ct - ps[i].at;
-    ps[i].wt = ps[i].tat - ps[i].bt;
-    ps[i].rt = ps[i].wt;
+    sum_tat += ps[i].tat;
+    sum_wt  += ps[i].wt;
+    sum_rt  += ps[i].rt;
 
-    sum_tat+=ps[i].tat;
-    sum_wt += ps[i].wt;
-    sum_rt += ps[i].rt;
-
-    idle_time+= (i==0)?0:ps[i].start_time-ps[i-1].ct;
+    idle_time+= (i==0)?0:ps[i].start_time-ps[i-1].ct; 
   }
-  qsort((void *)ps,n, sizeof(struct process_struct),comparatorPID);
-    length_cycle = ps[n-1].ct-ps[0].at;
 
-    throughput = (n/(float)length_cycle);
+  length_cycle = ps[n-1].ct-ps[0].at;
+  qsort((void *)ps,n,sizeof(struct process_struct),comparatorPID);
+  
+  cpu_utilisation = (float)(length_cycle-idle_time)/length_cycle;
 
-    cpu_utilisation = (float)((length_cycle-idle_time)/length_cycle);
-    printf("processID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
-    for(int i=0;i<n;i++)
-    {
-      printf("%d\t\t%d\t%d\t%d\t%d\t%d\t%d\t\n",ps[i].pid,ps[i].at,ps[i].bt,ps[i].ct,ps[i].tat,ps[i].wt,ps[i].rt);
-    }
-    printf("avg tat is %f \n",sum_tat/n);
-    printf("avg wt is %f \n",sum_wt/n);
-    printf("avg rt is %f \n",sum_rt/n);
-    printf("throughput is %f \n",throughput);
-    printf("cpu_utilisation is %f \n",cpu_utilisation*100);
+  throughput = (float)(n/length_cycle);
 
-    return 0;
+  printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+  for(int i=0;i<n;i++)
+  {
+    printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",ps[i].pid,ps[i].at,ps[i].bt,ps[i].ct,ps[i].tat,ps[i].wt,ps[i].rt);
+  }
 
+  printf("avg tat is %f\n",sum_tat/n);
+  printf("avg wt is %f\n",sum_wt/n);
+  printf("avg rt is %f\n",sum_rt/n);
+
+  printf("cpu_utilisation is %f\n",cpu_utilisation*100);
+  printf("throughput is %f\n",throughput);
+
+  return 0;
 }
