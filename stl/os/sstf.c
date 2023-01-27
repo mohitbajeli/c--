548518d2a2e4dbaf4526d8f0b0a7fc338df8fbed
mohitbajeli <mohitbajeli7617@gmail.com>
2023-01-27 17:23:12 +0530

    c

--- stl/os/sstf.c
+++ stl/os/sstf.c
@@ -13,7 +13,7 @@ int findclosestCylinder(int initial_pos, bool visited[],int request_queue[],int 
       if(visited[i] == false && abs(initial_pos - request_queue[i]) < min)
       {
           min = abs(initial_pos - request_queue[i]);
-          min_index = i;//gjjfhgfhgdfgsdfssdfsd
+          min_index = i;//gjjfhgfhgdfgsdfssdfsdhvjhhj
       }
   }
   return min_index;
